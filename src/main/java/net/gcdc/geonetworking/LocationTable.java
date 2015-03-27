78a63d9db2d315d09cafc0c80d9b9770203ab318
alexvoronov <alexey.voronov@gmail.com>
2015-03-27 22:33:06 +0100

    Update to use LocationTableListener

--- src/main/java/net/gcdc/geonetworking/LocationTable.java
+++ src/main/java/net/gcdc/geonetworking/LocationTable.java
@@ -22,7 +22,7 @@ public class LocationTable {
 
     private final ConfigProvider configProvider;
 
-    private final Collection<UpdateListener> listeners = new ArrayList<>();
+    private final Collection<LocationTableListener> listeners = new ArrayList<>();
 
     // Do these maps have to be synchronized? Collections.synchronizedMap or ConcurrentHashMap?
     private final Map<Address, Entry> gnMap = new ConcurrentHashMap<>();
@@ -36,17 +36,20 @@ public class LocationTable {
 
     /** Entry in a Location Table.
      *
-     * The flag 'locationServicePending' should set to false if not renewed within
+     * Entry is immutable at the moment. If there will be performance problem, or an inconvenience
+     * in removing old entries, Entry could be made mutable.
+     *
+     * TODO: the flag 'locationServicePending' should set to false if not renewed within
      *  3 × itsGnBeaconServiceRetransmitTimer
      */
     public static class Entry {
-        private Address address;
-        private MacAddress macAddress;
-        private LongPositionVector position;
-        private boolean locationServicePending;
-        private boolean isNeighbour;
-        private int sequenceNumber;
-        private Instant timestamp;
+        private final Address address;
+        private final MacAddress macAddress;
+        private final LongPositionVector position;
+        private final boolean locationServicePending;
+        private final boolean isNeighbour;
+        private final int sequenceNumber;
+        private final Instant timestamp;
 
         public Address address() { return address; }
         public MacAddress macAddress() { return macAddress; }
@@ -61,37 +64,47 @@ public class LocationTable {
         public Entry withIsNeighbour(boolean isNeighbour) { return new Builder(this).isNeighbour(isNeighbour).create(); }
         public Entry withTimestamp(Instant timestamp) { return new Builder(this).timestamp(timestamp).create(); }
 
-        private Entry() {}
+        private Entry(Builder builder) {
+            this.address = builder.address;
+            this.macAddress = builder.macAddress;
+            this.position = builder.position;
+            this.locationServicePending = builder.locationServicePending;
+            this.isNeighbour = builder.isNeighbour;
+            this.sequenceNumber = builder.sequenceNumber;
+            this.timestamp = builder.timestamp;
+        }
 
         public Builder builder() { return new Builder(); }
 
         public static class Builder {
-            private final Entry val;
+            private Address address;
+            private MacAddress macAddress;
+            private LongPositionVector position;
+            private boolean locationServicePending;
+            private boolean isNeighbour;
+            private int sequenceNumber;
+            private Instant timestamp;
+
             boolean created = false;
-            private Builder() { val = new Entry(); }
+            private Builder() { }
             private Builder(Entry entry) {
-                val = new Entry();
-                val.address = entry.address;
-                val.macAddress = entry.macAddress;
-                val.position = entry.position;
-                val.locationServicePending = entry.locationServicePending;
-                val.isNeighbour = entry.isNeighbour;
-                val.sequenceNumber = entry.sequenceNumber;
-                val.timestamp = entry.timestamp;
+                address = entry.address;
+                macAddress = entry.macAddress;
+                position = entry.position;
+                locationServicePending = entry.locationServicePending;
+                isNeighbour = entry.isNeighbour;
+                sequenceNumber = entry.sequenceNumber;
+                timestamp = entry.timestamp;
             }
-            private void checkCreated() {
-                if (created) { throw new IllegalStateException("Already created"); }
-            }
-            public Entry create() { created = true; return val; }
+            public Entry create() { created = true; return new Entry(this); }
 
-            public Builder address(Address address) { checkCreated(); val.address = address; return this; }
-            public Builder macAddress(MacAddress macAddress) { checkCreated(); val.macAddress = macAddress; return this; }
-            public Builder position(LongPositionVector position) { checkCreated(); val.position = position; return this; }
-            public Builder locationServicePending(boolean locationServicePending) { checkCreated(); val.locationServicePending = locationServicePending; return this; }
-            public Builder isNeighbour(boolean isNeighbour) { checkCreated(); val.isNeighbour = isNeighbour; return this; }
-            public Builder sequenceNumber(int sequenceNumber) { checkCreated(); val.sequenceNumber = sequenceNumber; return this; }
-            public Builder timestamp(Instant timestamp) { checkCreated(); val.timestamp = timestamp; return this; }
-
+            public Builder address(Address address) { this.address = address; return this; }
+            public Builder macAddress(MacAddress macAddress) { this.macAddress = macAddress; return this; }
+            public Builder position(LongPositionVector position) { this.position = position; return this; }
+            public Builder locationServicePending(boolean locationServicePending) { this.locationServicePending = locationServicePending; return this; }
+            public Builder isNeighbour(boolean isNeighbour) { this.isNeighbour = isNeighbour; return this; }
+            public Builder sequenceNumber(int sequenceNumber) { this.sequenceNumber = sequenceNumber; return this; }
+            public Builder timestamp(Instant timestamp) { this.timestamp = timestamp; return this; }
         }
 
         // type: vehicle or rsu
@@ -110,7 +123,7 @@ public class LocationTable {
         return null;
     }
 
-    // TODO: add a list of nodes-to-not-use (last forwarder)
+    // TODO: add a list of nodes-to-not-use (e.g. never return the last forwarder)
     public Optional<MacAddress> closerThanMeTo(Position destination, Position me) {
         Entry nearest = null;
         double shortestDistance = me.distanceInMetersTo(destination);
@@ -173,14 +186,10 @@ public class LocationTable {
         return gnMap.values();
     }
 
-    public static interface UpdateListener {
-        void onChange();
-    }
-
-    public void addListener(UpdateListener listener) { listeners.add(listener); }
-    public void removeListener(UpdateListener listener) { listeners.remove(listener); }
+    public void addListener(LocationTableListener listener) { listeners.add(listener); }
+    public void removeListener(LocationTableListener listener) { listeners.remove(listener); }
 
     private void notifyListeners() {
-        for (UpdateListener listener : listeners) { listener.onChange(); }
+        for (LocationTableListener listener : listeners) { listener.notifyStructureChanged(); }
     }
 }
