ae1d9d36770cae2c92c8000d6ef5c7a714c4d26b
heuvendw <dennis.heuven@tno.nl>
2016-05-09 18:04:11 +0200

    Alternative GPSD client using telnetclient

--- geonetworking/src/main/java/net/gcdc/geonetworking/gpsdclient/GpsdClient.java
+++ geonetworking/src/main/java/net/gcdc/geonetworking/gpsdclient/GpsdClient.java
@@ -1,12 +1,10 @@
 package net.gcdc.geonetworking.gpsdclient;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.InetSocketAddress;
-import java.net.Socket;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
@@ -17,6 +15,12 @@ import net.gcdc.geonetworking.Optional;
 import net.gcdc.geonetworking.Position;
 import net.gcdc.geonetworking.PositionProvider;
 
+import org.apache.commons.net.telnet.EchoOptionHandler;
+import org.apache.commons.net.telnet.InvalidTelnetOptionException;
+import org.apache.commons.net.telnet.SuppressGAOptionHandler;
+import org.apache.commons.net.telnet.TelnetClient;
+import org.apache.commons.net.telnet.TelnetNotificationHandler;
+import org.apache.commons.net.telnet.TerminalTypeOptionHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.threeten.bp.Instant;
@@ -25,52 +29,133 @@ import org.threeten.bp.OffsetDateTime;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
-public class GpsdClient implements PositionProvider, AutoCloseable {
+public class GpsdClient implements PositionProvider, AutoCloseable,TelnetNotificationHandler,Runnable {
     private final static Logger logger = LoggerFactory.getLogger(GpsdClient.class);
 
     private TPV                   lastSeenTPV = null;
-
     private final Gson            gson        = new GsonBuilder().create();
-    private final Socket          socket;  // GPSd uses TCP socket.
-    private final BufferedReader  rdr;
-    private final BufferedWriter  wrt;
     private final ExecutorService executor    = Executors.newSingleThreadExecutor();
-    private final Future<?>       runner;
+    private Future<?>       runner;
+    
+    private static TelnetClient tc = null;
 
+    @Override
+    public void run(){
+    	String remoteip = "127.0.0.1";
+    	int remoteport = 2947;
+    	
+        tc = new TelnetClient();
+
+        TerminalTypeOptionHandler ttopt = new TerminalTypeOptionHandler("VT100", false, false, true, false);
+        EchoOptionHandler echoopt = new EchoOptionHandler(true, false, true, false);
+        SuppressGAOptionHandler gaopt = new SuppressGAOptionHandler(true, true, true, true);
+        try{
+            tc.addOptionHandler(ttopt);
+            tc.addOptionHandler(echoopt);
+            tc.addOptionHandler(gaopt);
+        }
+        catch (InvalidTelnetOptionException e){
+            System.err.println("Error registering option handlers: " + e.getMessage());
+        } catch (IOException e) {
+			e.printStackTrace();
+		}
+
+        while (true){
+            try{
+                tc.connect(remoteip, remoteport);
+                tc.registerNotifHandler(this);
+                
+                createReader();
+                
+                OutputStream outstr = tc.getOutputStream();
+
+                Thread.sleep(1000);//Wait till ready
+                
+                String line = "?WATCH={\"enable\":true,\"json\":true}\r\n";                
+                byte[] buff = line.getBytes();
+                outstr.write(buff, 0 , buff.length);
+                outstr.flush();
+                
+                //Wait till finished or error occured
+                runner.get();
+                
+            }catch (IOException | InterruptedException | ExecutionException e){
+            	//System.err.println("Exception while connecting:" + e.getMessage());
+                
+            	//PANIC?
+            	logger.error("Exception while connecting:" + e.getMessage());
+            	
+            	//return;//set failsafe
+            	//System.exit(1);
+            }
+        }
+    }
+    
+    public void onMsgReceived(String line){
+    	try{
+	    	if (line.equals(""))
+	            return;
+	        if (line.startsWith("{\"class\":\"TPV\"")) {
+	            TPV msg = gson.fromJson(line, TPV.class);
+	            logger.info("tpv update:"+msg.time()+": ("+msg.lat()+","+msg.lon()+")");
+	            lastSeenTPV = msg;
+	        } else if (line.startsWith("{\"class\":\"SKY\"")) {
+	            @SuppressWarnings("unused")
+	            SKY msg = gson.fromJson(line, SKY.class);  // Ignore.
+	        } else if(line.startsWith("{\"class\":\"DEVICES\"")){
+	        	//DEVICES
+	    	} else if(line.startsWith("{\"class\":\"DEVICE\"")){
+	    		//DEVICE (activated)
+	    	}else if(line.startsWith("{\"class\":\"VERSION\"")){
+	    		//VERSION INFO
+	    	}else{
+	        	logger.debug("??:'"+line+"'");
+	        }
+    	}catch(com.google.gson.JsonSyntaxException j){
+    		//Parsing error
+    		logger.debug("Json parsing error :"+j.getMessage());//probably partial message
+    	}catch(NumberFormatException n){
+    		//ignore for printing
+    		logger.debug("Json parsing error (NumberFormatException) :"+n.getMessage());
+    	}catch(Exception e){
+    		e.printStackTrace();
+    		return;
+    	}
+    }
+    
+    private Future<?> createReader(){
+    	 runner = executor.submit(new Runnable() {
+             @Override public void run() {
+            	final int buffsize = 4096;
+ 	            InputStream instr = tc.getInputStream();
+ 	            try{
+ 	                byte[] buff = new byte[buffsize];
+ 	                int ret_read = 0;
+ 	
+ 	                do{
+ 	                    ret_read = instr.read(buff);
+ 	                    if(ret_read > 0){
+ 	                    	onMsgReceived(new String(buff, 0, ret_read));
+ 	                    }
+ 	                }while (ret_read >= 0);
+ 	            }catch (IOException e){
+ 	                System.err.println("Exception while reading socket:" + e.getMessage());
+ 	            }
+ 	            //STOP
+ 	            try{
+ 	                tc.disconnect();
+ 	                //STOPPING
+ 	            }
+ 	            catch (IOException e){
+ 	                System.err.println("Exception while closing telnet:" + e.getMessage());
+ 	            }
+             }
+         });
+    	 return runner;
+    }
+    
     public GpsdClient(InetSocketAddress address) throws IOException {
         logger.info("Starting GPSd client");
-        socket = new Socket(address.getAddress(), address.getPort());
-        rdr = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-        wrt = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
-
-        // Test with Plugtest server: "nc 212.234.160.4 1941"
-        wrt.write("?WATCH={\"enable\":true,\"json\":true};\n");  // Send us updates automatically.
-        wrt.flush();
-        runner = executor.submit(new Runnable() {
-            @Override public void run() {
-                // We don't need to poll with "?POLL;" command if "json":true was in ?WATCH.
-                while (rdr != null) {
-                    String line = null;
-                    try {
-                        line = rdr.readLine();  // GPSd speaks one long line per one message.
-                    } catch (IOException e) {
-                        // TODO Auto-generated catch block
-                        e.printStackTrace();
-                    }
-                    if (line == null)
-                        break;
-                    if (line.startsWith("{\"class\":\"TPV\"")) {
-                        TPV msg = gson.fromJson(line, TPV.class);
-                        lastSeenTPV = msg;
-                    } else if (line.startsWith("{\"class\":\"SKY\"")) {
-                        @SuppressWarnings("unused")
-                        SKY msg = gson.fromJson(line, SKY.class);  // Ignore.
-                    } else {
-                        // Ignore.
-                    }
-                }
-            }
-        });
     }
 
     @Override
@@ -99,11 +184,21 @@ public class GpsdClient implements PositionProvider, AutoCloseable {
 
     @Override
     public void close() throws IOException {
-        rdr.close();  // Needed?
-        wrt.close();
-        socket.close();
+    	//socket.close
+    	try{
+            tc.disconnect();
+        }
+        catch (IOException e){
+            System.err.println("Exception while closing telnet:" + e.getMessage());
+        }
+    	
         runner.cancel(true);
         executor.shutdownNow();
+    }
+    
+    public GpsdClient startClient(){
+    	(new Thread(this)).start();
+    	return this;
     }
 
     // Just a test main method.
@@ -120,18 +215,24 @@ public class GpsdClient implements PositionProvider, AutoCloseable {
             System.err.println(usage);
             System.exit(1);
         }
+        System.out.println("GPS: "+hostAndPort[0]+":"+hostAndPort[1]);
         InetSocketAddress remoteAddress =
                 new InetSocketAddress(hostAndPort[0], Integer.parseInt(hostAndPort[1]));
 
         try (GpsdClient client = new GpsdClient(remoteAddress)) {
-            while(true) {
-                System.out.println(client.getLatestPosition());
-                System.out.flush();
-                Thread.sleep(500);
+        	client.startClient();
+        	while(true) {
+                Thread.sleep(1000);
             }
         } catch (IOException | InterruptedException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
     }
+
+	@Override
+	public void receivedNegotiation(int arg0, int arg1) {
+		// TODO Auto-generated method stub
+		
+	}
 }
